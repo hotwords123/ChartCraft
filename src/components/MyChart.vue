41088de267cb511a09e9562eee0b113fb92d110f
hotwords123 <hotwords123@gmail.com>
2023-07-23 12:36:25 +0800

    refactor(chart): modularize

--- src/components/MyChart.vue
+++ src/components/MyChart.vue
@@ -7,33 +7,31 @@ const props = defineProps<{
   options: ChartOptions
 }>()
 
+/* ==== canvas DOM ==== */
+
 const canvasRef = ref<HTMLCanvasElement | null>(null)
-const ctxRef = ref<CanvasRenderingContext2D | null>(null)
+let ctx: CanvasRenderingContext2D | null = null
 
 onMounted(() => {
   const canvas = canvasRef.value
   if (!canvas) return
 
-  const ctx = canvas.getContext('2d')
+  ctx = canvas.getContext('2d')
   if (!ctx) return
 
-  ctxRef.value = ctx
-
   resizeCanvas()
-  render()
+  render(ctx)
 })
 
-let oldWidth = 0
-let oldHeight = 0
+watch(props, () => {
+  nextTick(() => ctx && render(ctx))
+})
 
 function resizeCanvas() {
   const canvas = canvasRef.value
-  const ctx = ctxRef.value
   if (!canvas || !ctx) return
 
   const { width, height } = props.options
-  oldWidth = width
-  oldHeight = height
 
   const devicePixelRatio = window.devicePixelRatio || 1
 
@@ -46,38 +44,107 @@ function resizeCanvas() {
   ctx.scale(devicePixelRatio, devicePixelRatio)
 }
 
-function render() {
-  const ctx = ctxRef.value
-  if (!ctx) return
+/* ==== line chart data ==== */
 
+let percentages: number[] = []
+
+function prepareData() {
+  const { data } = props
+
+  // generate line chart data points
+  const total = data.reduce((sum, [, y]) => sum + y, 0)
+  percentages = data.map(([, y]) => (y / total) * 100)
+}
+
+/* ==== chart layout ==== */
+
+let oldWidth = 0
+let oldHeight = 0
+
+let originX: number
+let originY: number
+let boxWidth: number
+let boxHeight: number
+
+let stepX: number
+let barStepY: number
+let lineOffsetY: number
+let lineStepY: number
+
+function calculateLayout() {
   const { data, options } = props
   const { width, height } = options
 
+  // calculate origin and box size
+  originX = 80
+  originY = height - 40
+  boxWidth = width - 70 - originX
+  boxHeight = originY - 80
+
+  // calculate step size
+  stepX = boxWidth / data.length
+
+  // for bar chart
+  const yMax = Math.max(...data.map(([, y]) => y))
+  barStepY = boxHeight / (1.4 * yMax)
+
+  // for line chart
+  const maxPercentage = Math.max(...percentages)
+  lineOffsetY = 80
+  lineStepY = (boxHeight - lineOffsetY) / (1.2 * maxPercentage)
+
+  // TODO: better step size calculation?
+  // TODO: adaptive tick values
+}
+
+/* ==== render ==== */
+
+function render(ctx: CanvasRenderingContext2D) {
+  const { options } = props
+  const { width, height } = options
+
+  prepareData()
+
   if (width !== oldWidth || height !== oldHeight) {
     resizeCanvas()
+
+    oldWidth = width
+    oldHeight = height
   }
+
+  calculateLayout()
 
   ctx.save()
   ctx.clearRect(0, 0, width, height)
-
-  // calculate origin and box size
-  const originX = 80
-  const originY = height - 40
-  const boxWidth = width - 70 - originX
-  const boxHeight = originY - 80
 
   // draw title
   ctx.textAlign = 'center'
   ctx.textBaseline = 'bottom'
   ctx.font = options.text.titleFont
   ctx.fillStyle = options.text.fillStyle
-  ctx.fillText(options.title, width / 2, 55)
+  ctx.fillText(options.title, width / 2, 55) // FIXME: hard-coded value
 
   ctx.translate(originX, originY)
 
   // draw background
   ctx.fillStyle = options.background
   ctx.fillRect(0, 0, boxWidth, -boxHeight)
+
+  drawAxes(ctx)
+
+  if (options.barChart.visible) {
+    drawBarChart(ctx)
+  }
+
+  if (options.lineChart.visible) {
+    drawLineChart(ctx)
+  }
+
+  ctx.restore()
+}
+
+function drawAxes(ctx: CanvasRenderingContext2D) {
+  const { data, options } = props
 
   // draw x-axis
   ctx.fillStyle = options.axes.color
@@ -101,18 +168,13 @@ function render() {
   ctx.fill()
   ctx.stroke()
 
-  // calculate layout
-  const xStep = boxWidth / data.length
-  const yMax = Math.max(...data.map(([, y]) => y))
-  const yStep = boxHeight / (1.4 * yMax)
-
   // draw x-axis labels
   ctx.textAlign = 'center'
   ctx.textBaseline = 'top'
   ctx.font = options.text.font
   ctx.fillStyle = options.text.fillStyle
   data.forEach(([x], i) => {
-    ctx.fillText(`${x}`, (i + 0.5) * xStep, 8)
+    ctx.fillText(`${x}`, (i + 0.5) * stepX, 8)
   })
 
   // draw x-axis title
@@ -122,111 +184,103 @@ function render() {
   // draw y-axis labels
   ctx.textAlign = 'right'
   ctx.textBaseline = 'middle'
+  const yMax = Math.max(...data.map(([, y]) => y))
   for (let i = 0; i <= yMax; i++) {
-    ctx.fillText(`${i}`, -8, -i * yStep)
+    ctx.fillText(`${i}`, -8, -i * barStepY)
   }
 
   // draw y-axis title
   ctx.fillText('Count', -12, -boxHeight)
+}
 
-  // draw bar chart
-  if (options.barChart.visible) {
+function drawBarChart(ctx: CanvasRenderingContext2D) {
+  const { data, options } = props
+
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'bottom'
+
+  let barFillStyle: string | CanvasGradient
+  if (typeof options.barChart.fillStyle === 'string') {
+    barFillStyle = options.barChart.fillStyle
+  } else {
+    const { stops } = options.barChart.fillStyle
+    const gradient = ctx.createLinearGradient(0, 0, 0, -boxHeight)
+    for (const { offset, color } of stops) {
+      gradient.addColorStop(offset, color)
+    }
+    barFillStyle = gradient
+  }
+
+  data.forEach(([, y], i) => {
+    // draw bar
+    ctx.fillStyle = barFillStyle
+    ctx.fillRect((i + 0.25) * stepX, 0, 0.5 * stepX, -y * barStepY)
+
+    // draw bar value
+    ctx.fillStyle = options.text.fillStyle
+    ctx.fillText(`${y}`, (i + 0.5) * stepX, -y * barStepY - 5)
+  })
+}
+
+function drawLineChart(ctx: CanvasRenderingContext2D) {
+  const { options } = props
+
+  // calculate layout
+  ctx.save()
+  ctx.translate(0, -lineOffsetY)
+
+  // draw lines
+  ctx.strokeStyle = options.lineChart.strokeStyle
+  ctx.lineWidth = options.lineChart.lineWidth
+  if (Array.isArray(options.lineChart.lineDash)) {
+    ctx.setLineDash(options.lineChart.lineDash)
+  } else {
+    ctx.setLineDash(DASH_PATTERNS[options.lineChart.lineDash])
+  }
+
+  ctx.beginPath()
+  percentages.forEach((y, i) => {
+    const x0 = (i + 0.5) * stepX
+    const y0 = -y * lineStepY
+    if (i === 0) {
+      ctx.moveTo(x0, y0)
+    } else {
+      ctx.lineTo(x0, y0)
+    }
+  })
+  ctx.stroke()
+  ctx.setLineDash([])
+
+  // draw markers
+  const { shape, size, fillStyle, strokeStyle, lineWidth } = options.lineChart.marker
+  percentages.forEach((y, i) => {
+    const x0 = (i + 0.5) * stepX
+    const y0 = -y * lineStepY
+
+    ctx.fillStyle = fillStyle
+    ctx.strokeStyle = strokeStyle
+    ctx.lineWidth = lineWidth
+    ctx.beginPath()
+    switch (shape) {
+      case 'circle':
+        ctx.arc(x0, y0, size, 0, 2 * Math.PI)
+        break
+      case 'square':
+        ctx.rect(x0 - size, y0 - size, 2 * size, 2 * size)
+        break
+    }
+    ctx.fill()
+    ctx.stroke()
+
+    // draw marker value
+    ctx.fillStyle = options.text.fillStyle
     ctx.textAlign = 'center'
     ctx.textBaseline = 'bottom'
-
-    let barFillStyle: string | CanvasGradient
-    if (typeof options.barChart.fillStyle === 'string') {
-      barFillStyle = options.barChart.fillStyle
-    } else {
-      const { stops } = options.barChart.fillStyle
-      const gradient = ctx.createLinearGradient(0, 0, 0, -boxHeight)
-      for (const { offset, color } of stops) {
-        gradient.addColorStop(offset, color)
-      }
-      barFillStyle = gradient
-    }
-
-    data.forEach(([, y], i) => {
-      // draw bar
-      ctx.fillStyle = barFillStyle
-      ctx.fillRect((i + 0.25) * xStep, 0, 0.5 * xStep, -y * yStep)
-
-      // draw bar value
-      ctx.fillStyle = options.text.fillStyle
-      ctx.fillText(`${y}`, (i + 0.5) * xStep, -y * yStep - 5)
-    })
-  }
-
-  // draw line chart
-  if (options.lineChart.visible) {
-    // generate data points
-    const total = data.reduce((sum, [, y]) => sum + y, 0)
-    const percentages = data.map(([, y]) => (y / total) * 100)
-
-    // calculate layout
-    const maxPercentage = Math.max(...percentages)
-    const yOffset = 80
-    const yStep = (boxHeight - yOffset) / (1.2 * maxPercentage)
-    ctx.save()
-    ctx.translate(0, -yOffset)
-
-    // draw lines
-    ctx.strokeStyle = options.lineChart.strokeStyle
-    ctx.lineWidth = options.lineChart.lineWidth
-    if (Array.isArray(options.lineChart.lineDash)) {
-      ctx.setLineDash(options.lineChart.lineDash)
-    } else {
-      ctx.setLineDash(DASH_PATTERNS[options.lineChart.lineDash])
-    }
-
-    ctx.beginPath()
-    percentages.forEach((y, i) => {
-      const x0 = (i + 0.5) * xStep
-      const y0 = -y * yStep
-      if (i === 0) {
-        ctx.moveTo(x0, y0)
-      } else {
-        ctx.lineTo(x0, y0)
-      }
-    })
-    ctx.stroke()
-    ctx.setLineDash([])
-
-    // draw markers
-    const { shape, size, fillStyle, strokeStyle, lineWidth } = options.lineChart.marker
-    percentages.forEach((y, i) => {
-      const x0 = (i + 0.5) * xStep
-      const y0 = -y * yStep
-
-      ctx.fillStyle = fillStyle
-      ctx.strokeStyle = strokeStyle
-      ctx.lineWidth = lineWidth
-      ctx.beginPath()
-      switch (shape) {
-        case 'circle':
-          ctx.arc(x0, y0, size, 0, 2 * Math.PI)
-          break
-        case 'square':
-          ctx.rect(x0 - size, y0 - size, 2 * size, 2 * size)
-          break
-      }
-      ctx.fill()
-      ctx.stroke()
-
-      // draw marker value
-      ctx.fillStyle = options.text.fillStyle
-      ctx.textAlign = 'center'
-      ctx.textBaseline = 'bottom'
-      ctx.fillText(`${y.toFixed(0)}%`, x0, y0 - 15)
-    })
-
-    ctx.restore()
-  }
+    ctx.fillText(`${y.toFixed(0)}%`, x0, y0 - 15)
+  })
 
   ctx.restore()
 }
-
-watch(props, () => nextTick(render))
 </script>
 
 <template>
